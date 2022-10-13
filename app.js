8a1c693884651d3fd740ed1a822ef821ee5a37a0
unknown <arjunagnihotrikanpur@gmail.com>
2022-10-13 19:51:49 +0530

    fc

---
+++ app.js
@@ -0,0 +1,110 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const ejs = require('ejs');
+
+
+// Database
+mongoose.connect(process.env.CONNECTION, () => console.log("Connected To Databse"));
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    loggedIn: Boolean,
+})
+const User = new mongoose.model("User", UserSchema);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view-engine', 'ejs');
+
+//  ---- Routes ----
+
+// Frontend
+app.get('/', (req, res) =>{
+    res.render('index.ejs');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs', {msg: ""});
+})
+
+
+
+// Backend
+// Create User
+app.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, 10),
+        loggedIn: false,
+    })
+    user.save((err, data) => {
+        if(!err) {
+            res.send(data);
+        } else {
+            res.send("ERR");
+        }
+    });
+    res.redirect('/login');
+})
+
+// Find All Users
+app.get('/users', async (req, res) => {
+    let users = await User.find({});
+    res.send(users);
+})
+
+// Login
+app.post('/login', async (req, res) => {
+    //email and password
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find user exist or not
+    User.findOne({ email: email })
+        .then(user => {
+            //if user not exist than return status 400
+            if (!user) return res.render('login.ejs', {msg: "No such user!"})
+
+            //if user exist than compare password
+            //password comes from the user
+            //user.password comes from the database
+            bcrypt.compare(password, user.password, async (err, data) => {
+                //if error than throw error
+                if (err) throw err
+
+                //if both match than you can do anything
+                if (data) {
+                    let doc = await User.findOneAndUpdate({email: req.body.email}, {loggedIn: true});
+                    res.redirect('/');
+                } else {
+                    res.render('login.ejs', {msg: "Incorrect credentials"});
+                }
+
+            })
+
+        })
+    
+})
+
+
+// Logout
+app.post('/logout', async (req, res) => {
+    let doc = await User.findOneAndUpdate({email: req.body.email}, {loggedIn: false});
+    res.redirect('/login');
+})
+
+
+
+// Server Listen
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
